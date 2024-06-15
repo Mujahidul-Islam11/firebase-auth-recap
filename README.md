1c4f5a8acf510c496f0bf8a9f6054c58b55bc881
Mohammad Dinar <web.dev.dinar@gmail.com>
2024-06-15 20:47:04 +0600

    Created google sign in using GoogleProvider & SignInWithPopUp method from firebase

--- README.md
+++ README.md
@@ -6,3 +6,12 @@
 <li>After createUserWithEmailAndPassword() method call .then and .catch</li>
 <li>Access the response from firebase inside .then(result => {console.log(result.user)})</li>
 </ul>
+<h3>Create user by google signIn</h3>
+<ul>
+<li>Call the GoogleAuthProvider:- const provider = GoogleAuthProvider()</li>
+<li>Import google sign method of firebase:- signInWithPopUp()</li>
+<li>And pass the auth and provider inside the signInWithPopUp as parameter</li>
+<li>Access the response from firebase inside .then(result => {console.log(result.user)}) and catch error .catch(error =>{
+    console.error(error)
+})</li>
+</ul>
--- src/components/Google_SignIn/Google_SignIn.jsx
+++ src/components/Google_SignIn/Google_SignIn.jsx
@@ -1,20 +1,23 @@
-import { signInWithPopup } from "firebase/auth";
-import { GoogleAuthProvider } from "firebase/auth/web-extension";
+import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import auth from "../../firebase.config";
+import { useState } from "react";
 
 const Google_SignIn = () => {
+    const [user, setUser] = useState();
     const provider = new GoogleAuthProvider();
   const handleGoogleSignIn = () => {
     signInWithPopup(auth, provider)
       .then((result) => {
-        console.log(result.user);
+        setUser(result.user)
       })
       .catch((err) => {
         console.log(err);
       });
   };
+  console.log(user)
   return (
     <div>
+        <h2>Welcome, {user.displayName}</h2>
       <button onClick={handleGoogleSignIn}>Sign In With Google</button>
     </div>
   );
