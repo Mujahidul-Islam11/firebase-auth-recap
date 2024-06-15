3ed32bf6ceac7ac0ebaf273bf62d787c4641a646
Mohammad Dinar <web.dev.dinar@gmail.com>
2024-06-15 23:08:29 +0600

    created a handler for facebook login

--- src/components/Facebook/FacebookSignIn.jsx
+++ src/components/Facebook/FacebookSignIn.jsx
@@ -1,10 +1,20 @@
+import { signInWithPopup, FacebookAuthProvider } from "firebase/auth";
+import auth from "../../firebase.config";
 
 const FacebookSignIn = () => {
-    return (
-        <div>
-            <button>Sign in with fb</button>
-        </div>
-    );
+  const provider = new FacebookAuthProvider();
+  signInWithPopup(auth, provider)
+  .then(result =>{
+    console.log(result.user)
+  })
+  .catch(error =>{
+    console.log(error)
+  })
+  return (
+    <div>
+      <button>Sign in with fb</button>
+    </div>
+  );
 };
 
 export default FacebookSignIn;
